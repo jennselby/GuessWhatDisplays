51c22d4bd78f01d7fa1c56284a1a4e472a5a6305
Jen Selby <jselby@nuevaschool.org>
2017-04-19 22:16:54 -0700

    The variables (and operators) exercise

---
+++ README.md
@@ -0,0 +1,5 @@
+What does the app display
+- when it starts up?
+- after the Change button is clicked?
+- after the Change More button is clicked?
+- after the Change More button is clicked a second time?
---
+++ variables.js
@@ -0,0 +1,72 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Button,
+  View
+} from 'react-native';
+
+export default class Variables extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        x: 5,
+        y: 2,
+        z: 1,
+    }
+  }
+  changeThings() {
+    this.setState({
+        x: this.state.y,
+        z: 6,
+        y: this.state.z,
+    });
+  }
+  changeMoreThings() {
+    this.setState({
+        x: this.state.z - 2,
+        y: Math.pow(this.state.x, 2),
+        z: 3 % this.state.x,
+    });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.variableValue}>
+          x is {this.state.x}
+        </Text>
+        <Text style={styles.variableValue}>
+          y is {this.state.y}
+        </Text>
+        <Text style={styles.variableValue}>
+          z is {this.state.z}
+        </Text>
+        <Button onPress={() => this.changeThings()} title="Change" />
+        <Button onPress={() => this.changeMoreThings()} title="Change More" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  variableValue: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+AppRegistry.registerComponent('Variables', () => Variables);
